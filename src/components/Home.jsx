ad8888926904c18812633377510052550a04e7ba
hasanulhasan <hasanlink71@yahoo.com>
2024-01-18 14:35:07 +0600

    project initiated

---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import StudentTable from "./StudentTable";
+
+const Home = () => {
+  return (
+    <div className="bg-[#172227] font-[Inter] text-white">
+         <Navbar/>
+        <Hero/>
+        <StudentTable/>
+        <Footer/>
+    </div>
+  );
+};
+
+export default Home;
